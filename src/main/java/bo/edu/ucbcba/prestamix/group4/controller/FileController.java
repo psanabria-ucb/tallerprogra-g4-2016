ffd7c94f537ae240487fa1f35ff434cb793fbef5
mauriValdi <mauricioo.valdi@hotmail.com>
2016-06-01 05:06:44 -0400

    Funcion de Buscar en Archivados y nombres de Columnas en Tabla visible

--- src/main/java/bo/edu/ucbcba/prestamix/group4/controller/FileController.java
+++ src/main/java/bo/edu/ucbcba/prestamix/group4/controller/FileController.java
@@ -1,6 +1,7 @@
 package bo.edu.ucbcba.prestamix.group4.controller;
 
 import bo.edu.ucbcba.prestamix.group4.dao.PrestamixEntityManager;
+import bo.edu.ucbcba.prestamix.group4.exceptions.ValidationException;
 import bo.edu.ucbcba.prestamix.group4.model.File;
 
 import javax.persistence.EntityManager;
@@ -36,4 +37,102 @@ public class FileController {
         entityManager.close();
         return response;
     }
+
+    public List<File> searchFilesById(String q)
+    {
+        int a;
+        if (q.matches("[0-9]+") && (q.length()<7)) {
+            if (q.isEmpty()) {
+                a = 0;
+            } else {
+                a = Integer.parseInt(q);
+            }
+            EntityManager entityManager = PrestamixEntityManager.createEntityManager();
+            TypedQuery<File> query = entityManager.createQuery("select f from File f WHERE f.id =  :a", File.class);
+            query.setParameter("a", a);
+            List<File> response = query.getResultList();
+            entityManager.close();
+            return response;
+        }
+        else {
+            throw new ValidationException("El campo debe ser un número para buscar por ese criterio, corrija el dato para continuar");
+        }
+    }
+
+
+    public List<File> searchFilesByClientName(String q)
+    {
+        EntityManager entityManager = PrestamixEntityManager.createEntityManager();
+        TypedQuery<File> query = entityManager.createQuery("select f from File f WHERE lower(f.nameCustomer) like :nameCustomer", File.class);
+        query.setParameter("nameCustomer", "%" + q.toLowerCase() + "%");
+        List<File> response = query.getResultList();
+        entityManager.close();
+        return response;
+    }
+
+    public List<File> searchFilesByPledge(String q)
+    {
+        EntityManager entityManager = PrestamixEntityManager.createEntityManager();
+        TypedQuery<File> query = entityManager.createQuery("select f from File f WHERE lower(f.codPledge) like :codPledge", File.class);
+        query.setParameter("codPledge", "%" + q.toLowerCase() + "%");
+        List<File> response = query.getResultList();
+        entityManager.close();
+        return response;
+    }
+
+    public List<File> searchFilesByAmount(String q)
+    {
+        int a;
+        if (q.matches("[0-9]+") && (q.length()<7)) {
+            if (q.isEmpty()) {
+                a = 0;
+            } else {
+                a = Integer.parseInt(q);
+            }
+            EntityManager entityManager = PrestamixEntityManager.createEntityManager();
+            TypedQuery<File> query = entityManager.createQuery("select f from File f WHERE f.amount =  :a", File.class);
+            query.setParameter("a", a);
+            List<File> response = query.getResultList();
+            entityManager.close();
+            return response;
+        }
+        else {
+            throw new ValidationException("El campo debe ser un número no mayor a 6 dígitos para buscar por ese criterio, corrija el dato para continuar");
+        }
+    }
+
+
+    public List<File> searchFilesByType(String q)
+    {
+        EntityManager entityManager = PrestamixEntityManager.createEntityManager();
+        TypedQuery<File> query = entityManager.createQuery("select f from File f WHERE lower(f.type) like :type", File.class);
+        query.setParameter("type", "%" + q.toLowerCase() + "%");
+        List<File> response = query.getResultList();
+        entityManager.close();
+        return response;
+    }
+
+    public List<File> searchFilesByStatus(String q)
+    {
+        EntityManager entityManager = PrestamixEntityManager.createEntityManager();
+        TypedQuery<File> query = entityManager.createQuery("select f from File f WHERE lower(f.status) like :status", File.class);
+        query.setParameter("status", "%" + q.toLowerCase() + "%");
+        List<File> response = query.getResultList();
+        entityManager.close();
+        return response;
+    }
+
+    public List<File> searchFilesByDate(String q)
+    {
+        EntityManager entityManager = PrestamixEntityManager.createEntityManager();
+        TypedQuery<File> query = entityManager.createQuery("select f from File f WHERE lower(f.date) like :date", File.class);
+        query.setParameter("date", "%" + q.toLowerCase() + "%");
+        List<File> response = query.getResultList();
+        entityManager.close();
+        return response;
+    }
+
+
+
+
 }
--- src/main/java/bo/edu/ucbcba/prestamix/group4/view/FilePawn.java
+++ src/main/java/bo/edu/ucbcba/prestamix/group4/view/FilePawn.java
@@ -1,28 +1,54 @@
 package bo.edu.ucbcba.prestamix.group4.view;
 
 import bo.edu.ucbcba.prestamix.group4.controller.FileController;
+import bo.edu.ucbcba.prestamix.group4.exceptions.ValidationException;
 import bo.edu.ucbcba.prestamix.group4.model.File;
 import bo.edu.ucbcba.prestamix.group4.model.Pawn;
 import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
+import com.sun.javafx.scene.control.skin.TableColumnHeader;
+import com.sun.prism.paint.*;
+import com.sun.prism.paint.Color;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
+import java.util.Vector;
 
 public class FilePawn extends JDialog {
+    private Object[] header = {"Id", "Nombre de Cliente", "Código de prenda", "Monto", "Tipo", "Fecha", "Estado"};
     private JPanel rootPanel;
-    private JTable fileTable;
+    private JTable fileTable = new JTable(null, header);
+    private JButton SearchFileButton;
+    private JTextField SearchFileTextField;
+    private JRadioButton idRadioButton;
+    private JRadioButton nombreClienteRadioButton;
+    private JRadioButton codigoPrendaRadioButton;
+    private JRadioButton montoRadioButton;
+    private JRadioButton tipoRadioButton;
+    private JRadioButton fechaRadioButton;
+    private JRadioButton estadoRadioButton;
     private final FileController fileController;
+
 
     public FilePawn(JFrame edit, Pawn p) {
         super(edit, "-ARCHIVADOS-", true);
         setContentPane(rootPanel);
-        setSize(500, 600);
-
+        setSize(800, 600);
         fileController = new FileController();
         save(p);
+
+
+        SearchFileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                populateSearchingTableFiles();
+            }
+        });
     }
 
     public void save(Pawn p) {
@@ -30,25 +56,69 @@ public class FilePawn extends JDialog {
             fileController.create(p.getId(), p.getNameCustomer(), p.getCodPledge(),
                     String.valueOf(p.getAmount()), p.getType(), p.getDate(), p.getStatus());
             populateTable();
-        }
-        else
-        {
+        } else {
             populateTable();
         }
     }
 
+
+    public void populateSearchingTableFiles() {
+        List<File> files = fileController.show();
+        if (idRadioButton.isSelected()) {
+
+            try {
+                files = fileController.searchFilesById(SearchFileTextField.getText());
+            } catch (ValidationException ex) {
+                JOptionPane.showMessageDialog(this, ex.getMessage(), "Error de Formato", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+        if (nombreClienteRadioButton.isSelected()) {
+            files = fileController.searchFilesByClientName(SearchFileTextField.getText());
+        }
+        if (codigoPrendaRadioButton.isSelected()) {
+            files = fileController.searchFilesByPledge(SearchFileTextField.getText());
+        }
+        if (montoRadioButton.isSelected()) {
+            try {
+                files = fileController.searchFilesByAmount(SearchFileTextField.getText());
+            } catch (ValidationException ex) {
+                JOptionPane.showMessageDialog(this, ex.getMessage(), "Error de Formato", JOptionPane.ERROR_MESSAGE);
+            }
+
+        }
+        if (tipoRadioButton.isSelected()) {
+            files = fileController.searchFilesByType(SearchFileTextField.getText());
+        }
+        if (estadoRadioButton.isSelected()) {
+            files = fileController.searchFilesByStatus(SearchFileTextField.getText());
+        }
+        if (fechaRadioButton.isSelected()) {
+            files = fileController.searchFilesByDate(SearchFileTextField.getText());
+        }
+        DefaultTableModel model = new DefaultTableModel(null, header);
+
+        fileTable.setModel(model);
+        for (File f : files) {
+            Object[] row = new Object[7];
+
+            row[0] = f.getId();
+            row[1] = f.getNameCustomer();
+            row[2] = f.getCodPledge();
+            row[3] = f.getAmount();
+            row[4] = f.getType();
+            row[5] = f.getDate();
+            row[6] = f.getStatus();
+            model.addRow(row);
+        }
+
+    }
+
+
     public void populateTable() {
         List<File> files = fileController.show();
-        DefaultTableModel model = new DefaultTableModel();
-        model.addColumn("ID");
-        model.addColumn("NOMBRE DEL CLIENTE");
-        model.addColumn("CODIGO DE LA PRENDA");
-        model.addColumn("MONTO");
-        model.addColumn("TIPO");
-        model.addColumn("FECHA");
-        model.addColumn("ESTADO");
-        fileTable.setModel(model);
+        DefaultTableModel model = new DefaultTableModel(null, header);
 
+        fileTable.setModel(model);
         for (File f : files) {
             Object[] row = new Object[7];
 
@@ -79,9 +149,47 @@ public class FilePawn extends JDialog {
      */
     private void $$$setupUI$$$() {
         rootPanel = new JPanel();
-        rootPanel.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+        rootPanel.setLayout(new GridLayoutManager(3, 8, new Insets(0, 0, 0, 0), -1, -1));
+        SearchFileButton = new JButton();
+        SearchFileButton.setText("Buscar");
+        rootPanel.add(SearchFileButton, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        idRadioButton = new JRadioButton();
+        idRadioButton.setSelected(true);
+        idRadioButton.setText("Id");
+        rootPanel.add(idRadioButton, new GridConstraints(2, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        SearchFileTextField = new JTextField();
+        rootPanel.add(SearchFileTextField, new GridConstraints(1, 2, 1, 6, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        nombreClienteRadioButton = new JRadioButton();
+        nombreClienteRadioButton.setText("Nombre Cliente");
+        rootPanel.add(nombreClienteRadioButton, new GridConstraints(2, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        codigoPrendaRadioButton = new JRadioButton();
+        codigoPrendaRadioButton.setText("Codigo prenda");
+        rootPanel.add(codigoPrendaRadioButton, new GridConstraints(2, 3, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        montoRadioButton = new JRadioButton();
+        montoRadioButton.setText("Monto");
+        rootPanel.add(montoRadioButton, new GridConstraints(2, 4, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        tipoRadioButton = new JRadioButton();
+        tipoRadioButton.setText("Tipo");
+        rootPanel.add(tipoRadioButton, new GridConstraints(2, 5, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        fechaRadioButton = new JRadioButton();
+        fechaRadioButton.setText("Fecha");
+        rootPanel.add(fechaRadioButton, new GridConstraints(2, 6, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        estadoRadioButton = new JRadioButton();
+        estadoRadioButton.setText("Estado");
+        rootPanel.add(estadoRadioButton, new GridConstraints(2, 7, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JScrollPane scrollPane1 = new JScrollPane();
+        rootPanel.add(scrollPane1, new GridConstraints(0, 0, 1, 8, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         fileTable = new JTable();
-        rootPanel.add(fileTable, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_WANT_GROW, null, new Dimension(150, 50), null, 0, false));
+        fileTable.setShowHorizontalLines(false);
+        scrollPane1.setViewportView(fileTable);
+        ButtonGroup buttonGroup;
+        buttonGroup = new ButtonGroup();
+        buttonGroup.add(idRadioButton);
+        buttonGroup.add(nombreClienteRadioButton);
+        buttonGroup.add(codigoPrendaRadioButton);
+        buttonGroup.add(montoRadioButton);
+        buttonGroup.add(tipoRadioButton);
+        buttonGroup.add(fechaRadioButton);
     }
 
     /**
